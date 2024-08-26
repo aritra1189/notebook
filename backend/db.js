065a42b954f532d116a6bbe786bb3980380a5049
aritra sharma <aritrasharma96542@gmail.com>
2024-08-26 10:08:24 +0530

    lllll

--- backend/db.js
+++ backend/db.js
@@ -1,2 +1,16 @@
-const mongoose=require("mongoose");
-const mongo_url=
+const mongoose = require("mongoose");
+
+const mongo_url = "mongodb://localhost:27017/";
+
+const connectMongo = async () => {
+    try {
+        await mongoose.connect(mongo_url)
+
+        console.log("Connected to MongoDB successfully");
+    } catch (err) {
+        console.error("Error connecting to MongoDB:", err);
+    }
+};
+
+module.exports = connectMongo;
+
--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
 const express=require("express");
+const connectMongo=require('./db');
+connectMongo();
+const app=express();
+const port=3000;
+app.get('/',(req,res)=>{
+    res.send("hello world");
+})
+app.listen(port,()=>{
+    console.log (`app running at http://localhost:${port}`);
+});
